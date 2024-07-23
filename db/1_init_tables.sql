6d88100934ffaa2775a31d64084dca554e32ae80
Daniel Plotnikov <daniilrew@gmail.com>
2024-07-23 19:29:43 +0500

    auth-23072024
    Дополнения

--- db/1_init_tables.sql
+++ db/1_init_tables.sql
@@ -1,25 +1,72 @@
--- create table "departments" (
---   "id" serial primary key,
---   "department_name" varchar(255) null,
---   "access_level" smallint not null, -- low priority
---   "created_at" timestamp not null default NOW(),
---   "updated_at" timestamp not null default NOW()
--- );
+create table "access_levels" (
+    "id" serial primary key,
+    "level_name" varchar(50) not null,
+    "can_create_reports" BOOLEAN DEFAULT FALSE,
+    "can_read_reports" BOOLEAN DEFAULT FALSE,
+    "can_answer_reports" BOOLEAN DEFAULT FALSE
+);
+INSERT INTO access_levels (level_name) VALUES ('Без доступа');
+INSERT INTO access_levels (level_name, can_create_reports) VALUES ('Только отправка', True);
+INSERT INTO access_levels (level_name, can_create_reports, can_read_reports) VALUES ('Отправка и просмотр', True, True);
 
--- create table "users" (
---   "id" serial primary key,
---   "employee_id" smallint not null,
---   "full_name" varchar(255) not null,
---   "email" varchar(255) not null,
---   "password" varchar(255) not null,
---   "department_id" INTEGER not null DEFAULT 0,
---   "access_level" smallint not null DEFAULT 0, -- high priority
---   "created_at" timestamp not null default NOW(),
---   "updated_at" timestamp not null default NOW(),
---   FOREIGN KEY (department_id) REFERENCES departments (id) ON DELETE CASCADE,
---   UNIQUE(email),
---   UNIQUE(employee_id)
--- );
+
+create table "departments" (
+  "id" serial primary key,
+  "department_name" varchar(255) not null,
+  "access_level" smallint not null DEFAULT 1,
+  "created_at" timestamp not null default NOW(),
+  "updated_at" timestamp not null default NOW(),
+  FOREIGN KEY (access_level) REFERENCES access_levels (id) ON DELETE CASCADE
+);
+INSERT INTO departments (department_name, access_level) VALUES ('Гости', 1);
+INSERT INTO departments (department_name, access_level) VALUES ('Сотрудники', 2);
+INSERT INTO departments (department_name, access_level) VALUES ('Администраторы', 3);
+
+
+create table "users" (
+  "id" serial primary key,
+  "employee_id" smallint not null,
+  "first_name" varchar(50) not null, -- имя
+  "middle_name" varchar(50), -- отчество
+  "last_name" varchar(50) not null, -- фамилия
+  "email" varchar(255) not null,
+  "password" varchar(32) not null, --md5
+  "department_id" INTEGER not null DEFAULT 1,
+  "created_at" timestamp not null default NOW(),
+  "updated_at" timestamp not null default NOW(),
+  FOREIGN KEY (department_id) REFERENCES departments (id) ON DELETE CASCADE,
+  UNIQUE(email),
+  UNIQUE(employee_id)
+);
+
+
+create table "priority_list" (
+    "id" serial primary key,
+    "priority_name" varchar(20) not null
+);
+INSERT INTO priority_list (priority_name) VALUES ('Низкий'), ('Обычный'), ('Высокий');
+
+
+create table "problems_list" (
+    "id" serial primary key,
+    "problem_name" varchar(20) not null
+);
+INSERT INTO problems_list (problem_name) VALUES ('Общее'), ('Электроника');
+
+
+
+create table "tickets" (
+    "id" serial primary key,
+    "uuid" uuid not null,
+    "reporter_id" INTEGER not null,
+    "priority_id" INTEGER not null DEFAULT 2,
+    "problem_id" INTEGER not null DEFAULT 1,
+    "text" varchar(1024) not null,
+    "created_at" timestamp not null default NOW(),
+    FOREIGN KEY (reporter_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (priority_id) REFERENCES priority_list (id) ON DELETE CASCADE,
+    FOREIGN KEY (problem_id) REFERENCES problems_list (id) ON DELETE CASCADE
+);
 
 create table "tickets_simple" (
     "id" serial primary key,
